c3fc75f5ddf726ce558cd86b1c2a4666cc9ca75e
Anton Höglund <antonjhoglund@hotmail.com>
2019-03-10 14:42:30 +0200

    Update navigation selection in MainActivity
    - Update navigation selection, so that the correct fragments are
      selected from the navigation bar.

--- app/src/main/java/com/github/anthogis/meno/MainActivity.java
+++ app/src/main/java/com/github/anthogis/meno/MainActivity.java
@@ -3,31 +3,46 @@ package com.github.anthogis.meno;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
+
+import java.util.Optional;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+            boolean itemSelected = false;
+
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
+                case R.id.navigation_categories:
+                    selectedFragment = new CategoriesFragment();
+                    itemSelected = true;
+                    break;
+                case R.id.navigation_add_expense:
+                    selectedFragment = new AddExpenseFragment();
+                    itemSelected = true;
+                    break;
+                case R.id.navigation_expense_list:
+                    selectedFragment = new ExpenseListFragment();
+                    itemSelected = true;
+                    break;
+                case R.id.navigation_chart:
+                    selectedFragment = new ChartsFragment();
+                    itemSelected = true;
+                    break;
             }
-            return false;
+
+            getSupportFragmentManager().beginTransaction().replace(
+                    R.id.fragment_container, selectedFragment).commit();
+
+            return itemSelected;
         }
     };
 
@@ -36,9 +51,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setOnNavigationItemSelectedListener(navListener);
     }
 
 }
