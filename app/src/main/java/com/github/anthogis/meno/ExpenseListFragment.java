37d3dfa984f83c8ea98e7e69bd3e8d006216b002
Anton Höglund <antonjhoglund@hotmail.com>
2019-04-20 09:44:16 +0300

    Add javadoc for ExpenseListFragment

--- app/src/main/java/com/github/anthogis/meno/ExpenseListFragment.java
+++ app/src/main/java/com/github/anthogis/meno/ExpenseListFragment.java
@@ -15,14 +15,36 @@ import java.util.List;
 
 import de.codecrafters.tableview.TableView;
 
+/**
+ * A fragment which displays the individual Expenses stored in the app.
+ *
+ * @author Anton Höglund
+ * @version 1.3
+ * @since 1.0
+ */
 public class ExpenseListFragment extends Fragment {
 
+    /**
+     * Calls onCreate in superclass, and sets the title of MainActivity.
+     * @param savedInstanceState see Android documentation for Fragment
+     */
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getActivity().setTitle(R.string.title_expense_list);
     }
 
+    /**
+     * Sets the content for this Fragment.
+     *
+     * Sets the layout for this Fragment, and sets the DataAdapter for the ExpenseTable
+     * in the layout.
+     *
+     * @param inflater see Android documentation for Fragment
+     * @param container see Android documentation for Fragment
+     * @param savedInstanceState see Android documentation for Fragment
+     * @return the view created by the inflater.
+     */
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
