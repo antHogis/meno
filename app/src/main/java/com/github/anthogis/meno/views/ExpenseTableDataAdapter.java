71a944edfc40efd918028ff061daaf82f92f5ece
Anton Höglund <antonjhoglund@hotmail.com>
2019-04-21 19:40:14 +0300

    Add javadoc for ExpenseTableDataAdapter

--- app/src/main/java/com/github/anthogis/meno/views/ExpenseTableDataAdapter.java
+++ app/src/main/java/com/github/anthogis/meno/views/ExpenseTableDataAdapter.java
@@ -14,13 +14,43 @@ import java.util.List;
 
 import de.codecrafters.tableview.TableDataAdapter;
 
+/**
+ * TableDataAdapter for ExpenseTable.
+ *
+ * TableDataAdapter for ExpenseTable, which shows data of Expenses in a three-column view of TextViews.
+ *
+ * @author Anton Höglund
+ * @version 1.0
+ * @since 1.0
+ */
 public class ExpenseTableDataAdapter extends TableDataAdapter<Expense> {
+
+    /**
+     * The size of the text in the TextViews of the ExpenseTable.
+     */
     private static final int TEXT_SIZE = 16;
 
+    /**
+     * Calls the super constructor with parameters Context, List< ? >
+     * @param context the context of the TableDataAdapter.
+     * @param data the data in the TableDataAdapter.
+     */
     public ExpenseTableDataAdapter(Context context, List<Expense> data) {
         super(context, data);
     }
 
+    /**
+     * Sets the data for a cell in the ExpenseTable.
+     *
+     * Sets the data for a cell in the ExpenseTable.
+     * Sets the ExpenseCategory of the Expense to a cell in column 0, the cost of the Expense to
+     * a cell in column 1, and the date of the expense in column 2.
+     *
+     * @param rowIndex see documentation for de.codecrafters.tableview.TableDataAdapter.
+     * @param columnIndex see documentation for de.codecrafters.tableview.TableDataAdapter.
+     * @param parentView see documentation for de.codecrafters.tableview.TableDataAdapter.
+     * @return see documentation for de.codecrafters.tableview.TableDataAdapter.
+     */
     @Override
     public View getCellView(int rowIndex, int columnIndex, ViewGroup parentView) {
         Expense expense = getRowData(rowIndex);
